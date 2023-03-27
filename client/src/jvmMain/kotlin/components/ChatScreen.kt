efac25125f952d48d067e62799885aa9de5a410f
Name <ahmad.awaab@gmail.com>
2023-03-27 16:26:43 -0400

    Added message-textfield & send-btn and impl'd their features

--- client/src/jvmMain/kotlin/components/ChatScreen.kt
+++ client/src/jvmMain/kotlin/components/ChatScreen.kt
@@ -1,14 +1,25 @@
 package components
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.DisposableEffect
-import androidx.compose.runtime.mutableStateListOf
-import androidx.compose.runtime.remember
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.OutlinedTextField
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Send
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.ahmad_hossain.desktopchatapp.common.ChatMessageOuterClass
 import com.ahmad_hossain.desktopchatapp.common.ChatMessageOuterClass.ChatMessage
 import io.grpc.ManagedChannelBuilder
+import kotlinx.coroutines.launch
 import util.ChatClient
 
 @Composable
@@ -21,17 +32,48 @@ fun ChatScreen(userName: String) {
     val chatClient = remember { ChatClient(userName, channel) }
     val chatMessages = remember { mutableStateListOf<ChatMessage>() }
 
+    LaunchedEffect(chatClient) {
+        chatClient.startChat { chatMessages.add(it) }
+    }
+
     DisposableEffect(Unit) {
         onDispose { chatClient.close() }
     }
 
-    LazyColumn {
-        items(chatMessages) {
-            when (it.type) {
-                ChatMessageOuterClass.MessageType.UNSPECIFIED -> TODO()
-                ChatMessageOuterClass.MessageType.CHAT -> TODO()
-                ChatMessageOuterClass.MessageType.JOIN -> TODO()
-                ChatMessageOuterClass.MessageType.LEAVE -> TODO()
+    Box(Modifier.fillMaxSize()) {
+        LazyColumn {
+            items(chatMessages) { msg ->
+                when (msg.type) {
+                    ChatMessageOuterClass.MessageType.UNSPECIFIED -> TODO()
+                    ChatMessageOuterClass.MessageType.CHAT -> TODO()
+                    ChatMessageOuterClass.MessageType.JOIN -> TODO()
+                    ChatMessageOuterClass.MessageType.LEAVE -> TODO()
+                }
+            }
+        }
+        var message by remember { mutableStateOf("") }
+        val scope = rememberCoroutineScope()
+        Row(
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(start = 16.dp, end = 16.dp, bottom = 16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            OutlinedTextField(
+                modifier = Modifier.weight(1f),
+                value = message,
+                onValueChange = { message = it },
+                label = { Text("Send Message") }
+            )
+            IconButton(
+                onClick = {
+                    scope.launch {
+                        chatClient.sendMessage(message)
+                        message = ""
+                    }
+                }
+            ) {
+                Icon(Icons.Default.Send, null)
             }
         }
     }
