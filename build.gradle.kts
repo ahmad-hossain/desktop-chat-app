2fda93572e58cd0c7774e20d8f8494faff9504e2
Name <ahmad.awaab@gmail.com>
2023-03-27 20:40:21 -0400

    Restored original gradle files changed while debugging generated-protos not visible

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,56 +1,27 @@
-@file:Suppress("DSL_SCOPE_VIOLATION")
-
 plugins {
     kotlin("jvm") version "1.8.0"
     application
-//    alias(libs.plugins.kotlin) apply false
 }
 
-group = "dev.baseio.slackclone"
-version = "1.0"
+group = "org.example"
+version = "1.0-SNAPSHOT"
 
-subprojects {
-//    apply(plugin = rootProject.libs.plugins.ktlint.get().pluginId)
+repositories {
+    mavenCentral()
 }
 
-fun teamPropsFile(propsFile: String): File {
-    val teamPropsDir = file("team-props")
-    return File(teamPropsDir, propsFile)
+dependencies {
+    testImplementation(kotlin("test"))
 }
 
-allprojects {
-    repositories {
-        google()
-        gradlePluginPortal()
-        mavenLocal()
-        mavenCentral()
-        maven { setUrl("https://jitpack.io") }
-    }
+tasks.test {
+    useJUnitPlatform()
 }
-//plugins {
-//    kotlin("jvm") version "1.8.0"
-//    application
-//}
-//
-//group = "org.example"
-//version = "1.0-SNAPSHOT"
-//
-//repositories {
-//    mavenCentral()
-//}
-//
-//dependencies {
-//    testImplementation(kotlin("test"))
-//}
-//
-//tasks.test {
-//    useJUnitPlatform()
-//}
-//
-//kotlin {
-//    jvmToolchain(19)
-//}
-//
-//application {
-//    mainClass.set("MainKt")
-//}
+
+kotlin {
+    jvmToolchain(19)
+}
+
+application {
+    mainClass.set("MainKt")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,25 +1,3 @@
-pluginManagement {
-    repositories {
-        gradlePluginPortal()
-        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
-        google()
-        jcenter()
-        mavenCentral()
-        mavenLocal()
-        maven {
-            setUrl("https://repo1.maven.org/maven2/")
-        }
-        maven { setUrl("https://jitpack.io") }
-    }
-}
-
-dependencyResolutionManagement {
-    versionCatalogs {
-        create("libs") { from(files("./gradle/versions.toml")) }
-    }
-}
-// ----
-
 rootProject.name = "Desktop_Chat_App"
 
 include(":protos")
